b5a0fa14588b05f608bbdc4768584788d66f0b98
Jakiboy <j.sinnaour.official@gmail.com>
2020-05-13 07:41:12 +0000

    Update View.php

--- src/lib/View.php
+++ src/lib/View.php
@@ -26,17 +26,17 @@ class View extends PluginOptions implements ViewInterface
      */
     private $callables = false;
 
-	/**
-	 * Define custom callables
-	 *
-	 * @access public
+    /**
+     * Define custom callables
+     *
+     * @access public
      * @param array $callables
-	 * @return void
-	 */
-	public function setCallables($callables = [])
-	{
-		$this->callables = $callables;
-	}
+     * @return void
+     */
+    public function setCallables($callables = [])
+    {
+        $this->callables = $callables;
+    }
 
     /**
      * Render view
@@ -50,16 +50,16 @@ class View extends PluginOptions implements ViewInterface
         echo $this->assign($content, $template);
     }
 
-	/**
-	 * Aassign content to view
-	 *
+    /**
+     * Aassign content to view
+     *
      * @access public
-	 * @param array $content
+     * @param array $content
      * @param string $template
-	 * @return string
-	 */
-	public function assign($content = [], $template = 'default')
-	{
+     * @return string
+     */
+    public function assign($content = [], $template = 'default')
+    {
         // Set View environment
         $env = Template::getEnvironment($this->getPath($template),[
             'cache' => $this->getCachePath(),
@@ -73,7 +73,7 @@ class View extends PluginOptions implements ViewInterface
             }
         }
     
-		// Add view global functions
+        // Add view global functions
         $env->addFunction(Template::extend('dump', function ($var){
             var_dump($var);
         }));
@@ -125,20 +125,20 @@ class View extends PluginOptions implements ViewInterface
         $env->addFunction(Template::extend('unserialize', function ($string){
             return Data::unserialize($string);
         }));
-        $env->addFunction(Template::extend('hasFilter', function ($action){
-            return $this->hasFilter($action);
+        $env->addFunction(Template::extend('hasFilter', function ($hook){
+            return $this->hasFilter($hook);
         }));
-        $env->addFunction(Template::extend('applyFilter', function ($action){
-            return $this->applyFilter($action);
+        $env->addFunction(Template::extend('applyFilter', function ($hook, $value){
+            return $this->applyFilter($hook,$value);
         }));
-        $env->addFunction(Template::extend('doAction', function ($action){
-            $this->doPluginAction($action);
+        $env->addFunction(Template::extend('doAction', function ($hook){
+            $this->doPluginAction($hook);
         }));
 
         // Return rendered view
-		$view = $env->load("{$template}{$this->getViewExtension()}");
-		return $view->render($content);
-	}
+        $view = $env->load("{$template}{$this->getViewExtension()}");
+        return $view->render($content);
+    }
 
     /**
      * Get view path
