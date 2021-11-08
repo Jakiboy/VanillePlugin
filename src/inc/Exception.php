29d646b2bea2697353f8c7cd9a038f736adff70d
Jakiboy <j.sinnaour.official@gmail.com>
2021-11-08 07:15:41 +0100

    Update

--- src/inc/Exception.php
+++ src/inc/Exception.php
@@ -2,7 +2,7 @@
 /**
  * @author    : JIHAD SINNAOUR
  * @package   : VanillePlugin
- * @version   : 0.7.4
+ * @version   : 0.7.5
  * @copyright : (c) 2018 - 2021 JIHAD SINNAOUR <mail@jihadsinnaour.com>
  * @link      : https://jakiboy.github.io/VanillePlugin/
  * @license   : MIT
