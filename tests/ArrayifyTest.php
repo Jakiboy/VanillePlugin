32c73b3b1ded53e87a0ca71ad73aad7dc90f3f71
JIHAD SINNAOUR <j.sinnaour.official@gmail.com>
2022-03-08 22:57:31 +0100

    Update Version

--- tests/ArrayifyTest.php
+++ tests/ArrayifyTest.php
@@ -2,7 +2,7 @@
 /**
  * @author    : JIHAD SINNAOUR
  * @package   : VanillePlugin
- * @version   : 0.7.5
+ * @version   : 0.7.6
  * @copyright : (c) 2018 - 2021 JIHAD SINNAOUR <mail@jihadsinnaour.com>
  * @link      : https://jakiboy.github.io/VanillePlugin/
  * @license   : MIT
