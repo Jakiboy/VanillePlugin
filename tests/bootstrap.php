c8e75ea57943d176ded9ee1241f294dcf577199d
JIHAD SINNAOUR <j.sinnaour.official@gmail.com>
2022-03-31 16:52:16 +0000

    Update

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,7 +2,7 @@
 /**
  * @author    : JIHAD SINNAOUR
  * @package   : VanillePlugin
- * @version   : 0.7.6
+ * @version   : 0.7.7
  * @copyright : (c) 2018 - 2022 JIHAD SINNAOUR <mail@jihadsinnaour.com>
  * @link      : https://jakiboy.github.io/VanillePlugin/
  * @license   : MIT
