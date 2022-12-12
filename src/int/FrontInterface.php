5567fd83f90864388f69b113bdeb0de858373e70
JIHAD SINNAOUR <j.sinnaour.official@gmail.com>
2022-12-12 18:06:30 +0100

    Update version

--- src/int/FrontInterface.php
+++ src/int/FrontInterface.php
@@ -3,7 +3,7 @@
  * @author    : JIHAD SINNAOUR
  * @package   : VanillePlugin
  * @version   : 0.9.3
- * @copyright : (c) 2018 - 2022 JIHAD SINNAOUR <mail@jihadsinnaour.com>
+ * @copyright : (c) 2018 - 2023 JIHAD SINNAOUR <mail@jihadsinnaour.com>
  * @link      : https://jakiboy.github.io/VanillePlugin/
  * @license   : MIT
  *
